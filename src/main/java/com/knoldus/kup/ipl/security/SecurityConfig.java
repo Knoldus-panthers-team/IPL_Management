5c7063b5b3364b094c2b19113659e796a12b8b36
= <aasif.ali@knoldus.com>
2022-05-24 11:14:41 +0530

    removed unused code

--- src/main/java/com/knoldus/kup/ipl/security/SecurityConfig.java
+++ src/main/java/com/knoldus/kup/ipl/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -19,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
         userDetailsManager.createUser(User
                 .withUsername("admin")
-                .password("admin@12")
+                .password("$2a$10$2KXy6icbupG38tEnTeSPvuygSO8WykD0hGQ.Kj/yk.Rust7BfhZu6")
                 .roles("ADMIN")
                 .build());
         return userDetailsManager;
@@ -27,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Bean
     public PasswordEncoder getPassword(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
     
     @Override
