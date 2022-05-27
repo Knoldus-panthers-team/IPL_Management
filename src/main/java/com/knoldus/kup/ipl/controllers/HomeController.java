3d4a59df9e6d6aedf51a79f725b78e3b6331393d
KRISHNA-JAIS <krishna.jaiswal@knoldus.com>
2022-05-27 12:02:29 +0530

    Java Doc for HomeController

--- src/main/java/com/knoldus/kup/ipl/controllers/HomeController.java
+++ src/main/java/com/knoldus/kup/ipl/controllers/HomeController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    /**
+     *
+     * @return home page  .
+     */
     @GetMapping
     public String getHomepage() {
         return "home";
